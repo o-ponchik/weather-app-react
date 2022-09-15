cf608913c750d4b479a6070085bebd7d7ddde12d
o-ponchik <105226321+o-ponchik@users.noreply.github.com>
2022-09-16 00:26:43 +0300

    added_links_to_GotHub_and_Portfolio

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ function App() {
       <div className="container shadow">
         <Weather />
       </div>
+      <p>
+        <a href="https://github.com/o-ponchik" alt="GitHub">
+          Open-source code
+        </a>
+        , built by
+        <a href="https://lighthearted-pony-a59add.netlify.app/" alt="Portfolio">
+          Mariia Matviian
+        </a>
+      </p>
     </div>
   );
 }
