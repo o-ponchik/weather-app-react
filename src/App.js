8ac312f53eff9f62a562e8410bdce0af3e66dcfe
o-ponchik <105226321+o-ponchik@users.noreply.github.com>
2022-09-16 00:38:32 +0300

    decorated_links

--- src/App.js
+++ src/App.js
@@ -4,18 +4,7 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <div className="container shadow">
-        <Weather />
-      </div>
-      <p>
-        <a href="https://github.com/o-ponchik" alt="GitHub">
-          Open-source code
-        </a>
-        , built by
-        <a href="https://lighthearted-pony-a59add.netlify.app/" alt="Portfolio">
-          Mariia Matviian
-        </a>
-      </p>
+      <Weather />
     </div>
   );
 }
